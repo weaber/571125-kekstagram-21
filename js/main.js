05ca158a21b127e5212c3e4ed9a96875daf095cc
weaber <pnyxer@mail.ru>
2022-12-22 10:01:22 +0300

    Реализовал валидацию формы

--- js/main.js
+++ js/main.js
@@ -166,8 +166,11 @@ const closeImageEditForm = () => {
   uploadCancelElement.removeEventListener(`keydown`, uploadCancelElementEnterPressHandler);
 };
 
+const hashtagsInput = document.querySelector(`.text__hashtags`);
+const commentInput = document.querySelector(`.text__description`);
+
 const imageEditFormEscapePressHandler = (evt) => {
-  if (evt.key === `Escape`) {
+  if (evt.key === `Escape` && hashtagsInput !== document.activeElement && commentInput !== document.activeElement) {
     evt.preventDefault();
     document.removeEventListener(`keydown`, imageEditFormEscapePressHandler);
     closeImageEditForm();
@@ -191,3 +194,57 @@ const uploadCancelElementClickHandler = () => {
 };
 
 uploadFileInput.addEventListener(`change`, uploadFileInputClickHandler);
+
+const validateHashtags = () => {
+  if (hashtagsInput.value) {
+    const hashtags = hashtagsInput.value.trim().toLowerCase().split(` `);
+    const HASHTAG_MIN_LENGTH = 2;
+    const HASHTAG_MAX_LENGTH = 7;
+    const HASHTAGS_MAX_AMOUNT = 3;
+    const regular = /^#[\w]*$/;
+
+    const checkDuplicates = (array) => {
+      return (new Set(array)).size !== array.length;
+    };
+
+    const checkMinLength = (hashTags) => {
+      return hashTags.some((element) => element.length < HASHTAG_MIN_LENGTH);
+    };
+
+    const checkMaxLength = (hashTags) => {
+      return hashTags.some((element) => element.length > HASHTAG_MAX_LENGTH);
+    };
+
+    const checkHashtagMask = (hashTags) => {
+      return hashTags.some((element) => !regular.test(element));
+    };
+
+    if (hashtags.length > HASHTAGS_MAX_AMOUNT) {
+      return hashtagsInput.setCustomValidity(`Количество Хэштэгов не может быть больше ${HASHTAGS_MAX_AMOUNT}`);
+    }
+
+    if (checkDuplicates(hashtags)) {
+      return hashtagsInput.setCustomValidity(`Хэштэги не должны повторяться`);
+    }
+
+    if (checkMinLength(hashtags)) {
+      return hashtagsInput.setCustomValidity(`Хэштэг не может состоять только из #`);
+    }
+
+    if (checkMaxLength(hashtags)) {
+      return hashtagsInput.setCustomValidity(`Длина Хэштэга не может быть больше ${HASHTAG_MAX_LENGTH}`);
+    }
+
+    if (checkHashtagMask(hashtags)) {
+      const index = hashtags.findIndex((element) => !regular.test(element));
+      return hashtagsInput.setCustomValidity(`Хэштэг ${hashtags[index]} не соответствует маске`);
+    }
+  }
+  return hashtagsInput.setCustomValidity(``);
+};
+
+const hashtagsInputHandler = () => {
+  validateHashtags();
+};
+
+hashtagsInput.addEventListener(`input`, hashtagsInputHandler);
